e7ffb017919e712cfb64fac23e057b4b7878f78e
Dadabek Rajabov <barajabov@gmail.com>
2025-03-04 13:21:48 -0500

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = () => ['Antonia','Nuru']
+
+const returnLastTwoDrivers = () => ['Amari','Mo']
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+let firstElement = selectingDrivers[0]
+let secondElement = selectingDrivers[1]
+
+const createFareMultiplier = function(num1){
+    return function (num2){
+        return num1 * num2
+    }
+}
+
+const fareDoubler = createFareMultiplier (2)
+
+const fareTripler = createFareMultiplier (3)
+
+function selectDifferentDrivers(driversArray, fn){
+    return fn(driversArray)
+}
